6bd6575e4be4a6674ad2c56f32077b35fa859688
Hanbyul Kang <kanghanstar@outlook.com>
2022-01-07 10:54:02 +0900

    셀 움직임 메서드 변경

--- models/GridCell.ts
+++ models/GridCell.ts
@@ -17,9 +17,15 @@ class GridCell implements IGridCell {
         this.id = Math.random().toString();
     }
 
+    /**
+     * 현재 셀을 해당 숫자들 만큼 더해서 움직임
+     * @param x x 축으로 움직일 수
+     * @param y y 축에 더할 숫자
+     * @returns 성공 여부
+     */
     moveAxios(x: number, y: number): boolean {
-        this.x_axis = x;
-        this.y_axis = y;
+        this.x_axis = this.x_axis + x;
+        this.y_axis = this.y_axis + y;
 
         return true;
     }
