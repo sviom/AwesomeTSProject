54dd1fe5599dab6e0e4690f5819a2848c3a5cc47
Hanbyul Kang <kanghanstar@outlook.com>
2022-01-23 23:56:07 +0900

    제스처 라이브러리 및 제스처에 따른 기본값 설정

--- components/2048/GridView.tsx
+++ components/2048/GridView.tsx
@@ -10,12 +10,21 @@ import {
 	StyleProp,
 	ViewStyle,
 	Button,
+	PanResponderGestureState,
 } from 'react-native';
 import { Colors } from 'react-native/Libraries/NewAppScreen';
 import { GridCell } from '.';
 import { GridCellModel, GridRowModel } from '../../models';
 import { getRandomNumber } from '../../utils/NumberUtil';
 import { findEmptyCells } from './2048Rules';
+import GestureRecognizer from 'react-native-swipe-gestures';
+
+const swipeDirections = {
+	SWIPE_UP: 'SWIPE_UP',
+	SWIPE_DOWN: 'SWIPE_DOWN',
+	SWIPE_LEFT: 'SWIPE_LEFT',
+	SWIPE_RIGHT: 'SWIPE_RIGHT',
+};
 
 const styles = StyleSheet.create({
 	card: {
@@ -63,21 +72,48 @@ const GridView: React.FC<IProps> = () => {
 		setGrid(newArray);
 	};
 
-	return (
-		<View style={styles.card}>
-			<Text>{Math.random()}</Text>
-			{gridArray.map((row, row_index) => {
-				return (
-					<View style={styles.row} key={`row_${Math.random()}`}>
-						{row.GridCells.map((cell, column_index) => {
-							return <GridCell item={cell} key={`test_${Math.random()}`}></GridCell>;
-						})}
-					</View>
-				);
-			})}
+	const onSwipe = (gestureName: string, gestureState: PanResponderGestureState) => {
+		const { SWIPE_UP, SWIPE_DOWN, SWIPE_LEFT, SWIPE_RIGHT } = swipeDirections;
+		switch (gestureName) {
+			case SWIPE_UP:
+				// 왼쪽으로 돌리기 -> 왼쪽으로 붙이기 -> 합치기 -> 왼쪽으로 붙이기 -> 오른쪽으로 돌리기
+				// swipeFn(rotateRight(move(combine(move(rotateLeft(boxData))))));
+				break;
+			case SWIPE_DOWN:
+				// swipeFn(rotateLeft(move(combine(move(rotateRight(boxData))))));
+				break;
+			case SWIPE_LEFT:
+				// 왼쪽으로 붙이기 -> 합치기 -> 왼쪽으로 붙이기
+				// swipeFn(move(combine(move(boxData))));
+				break;
+			case SWIPE_RIGHT:
+				// swipeFn(reverse2d(move(combine(move(reverse2d(boxData))))));
+				break;
+		}
+	};
 
-			<Button title="ddfdfd" onPress={addTodo} />
-		</View>
+	return (
+		<GestureRecognizer
+			onSwipe={(direction, state) => onSwipe(direction, state)}
+			config={{
+				velocityThreshold: 0.3,
+				directionalOffsetThreshold: 80,
+			}}>
+			<View style={styles.card}>
+				<Text>{Math.random()}</Text>
+				{gridArray.map((row, row_index) => {
+					return (
+						<View style={styles.row} key={`row_${Math.random()}`}>
+							{row.GridCells.map((cell, column_index) => {
+								return <GridCell item={cell} key={`test_${Math.random()}`}></GridCell>;
+							})}
+						</View>
+					);
+				})}
+
+				<Button title="ddfdfd" onPress={addTodo} />
+			</View>
+		</GestureRecognizer>
 	);
 };
 
